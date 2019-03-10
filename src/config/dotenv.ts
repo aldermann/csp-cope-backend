b9b98a9b3d03e656bff206f4df7e5e32a9e23f1a
AnTran <vietanisme@gmail.com>
2019-03-10 23:09:30 +0700

    Full CI

--- src/config/dotenv.ts
+++ src/config/dotenv.ts
@@ -1,10 +1,11 @@
 import dotenv from "dotenv";
 if (process.env.NODE_ENV === "testing") {
     dotenv.config({ path: ".env.testing" });
-    // console.log(process.env.MONGO_URL);
 } else if (process.env.NODE_ENV === "development") {
     dotenv.config({path: ".env.development"});
-    // console.log (process.env.MONGO_URL);
 } else {
     dotenv.config({ path: ".env.production" });
 }
+dotenv.config({ path: ".env.secret" });
+console.log(process.env.MONGO_URL);
+console.log(process.env.SESSION_SECRET);
