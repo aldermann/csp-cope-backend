da3d47955a1fda62f13e4767b0245eb8c6a7a803
AnTran <vietanisme@gmail.com>
2019-03-11 22:41:10 +0700

    Test ssh on Travis

--- src/config/dotenv.ts
+++ src/config/dotenv.ts
@@ -1,11 +1,9 @@
 import dotenv from "dotenv";
 if (process.env.NODE_ENV === "testing") {
-    dotenv.config({ path: ".env.testing" });
+    dotenv.config({ path: "env/.env.testing" });
 } else if (process.env.NODE_ENV === "development") {
-    dotenv.config({path: ".env.development"});
+    dotenv.config({path: "env/.env.development"});
 } else {
-    dotenv.config({ path: ".env.production" });
+    dotenv.config({ path: "env/.env.production" });
 }
-dotenv.config({ path: ".env.secret" });
-console.log(process.env.MONGO_URL);
-console.log(process.env.SESSION_SECRET);
+dotenv.config({ path: "env/.env.secret" });
